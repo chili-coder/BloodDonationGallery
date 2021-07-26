1e20853f19dbb07d84ffe9e57fc0bfd8d2ad3464
Md Sohagh Mia <sohaghapk@gmail.com>
2021-07-26 13:04:47 +0600

    navigation
    navigation ui

--- app/src/main/java/com/sohaghlab/blooddonationgallery/MainActivity.java
+++ app/src/main/java/com/sohaghlab/blooddonationgallery/MainActivity.java
@@ -1,17 +1,42 @@
 package com.sohaghlab.blooddonationgallery;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView heelo;
+  private DrawerLayout drawerLayout;
+  public Toolbar toolbar;
+  private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar(toolbar);
+
+        /// getSupportActionBar().setTitle("BDG");
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        toolbar = findViewById(R.id.toolbar_main);
+        navigationView=findViewById(R.id.navigationbar);
+
+
+       ActionBarDrawerToggle  toggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout
+               , toolbar , R.string.navigation_draw_open,R.string.navigation_draw_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+
     }
 }
